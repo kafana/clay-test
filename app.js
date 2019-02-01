41da993a30077abfab973ffb4d08dd6d8c2770a8
Boris Musa <boris@kafana.org>
2019-01-31 21:36:58 -0500

    Added start script

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-var amphora = require('amphora'),
+/* var amphora = require('amphora'),
   port = process.env.PORT || 3000;
 
 return amphora()
   .then(function (server) {
     server.listen(port);
   });
+*/
+
+var app = require('express')(),
+  amphora = require('amphora'),
+  amphoraHtml = require('amphora-html'),
+  port = process.env.PORT || 3000,
+  env;
+
+// add project-specific settings to your app
+app.set('strict routing', true);
+app.set('trust proxy', 1);
+
+// add custom settings to your templating engine
+// env.addGlobal('projectName', process.env.PROJECT_NAME);
+
+return amphora({
+  app: app,
+  storage: require('amphora-storage-postgres'),
+  renderers: {
+    default: 'html',
+    html: amphoraHtml
+  }
+}).then(function (server) {
+  server.listen(port);
+});
+
